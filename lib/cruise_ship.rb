33c079f3b058c6aa1d1e3d6aa422db265a643f5c
John Kyeremeh <john.a.kyeremeh@gmail.com>
2021-05-12 11:10:17 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -18,19 +18,3 @@ def select_winner(passengers)
   winner
 end
 
-# Our job is to write a method that operates on a hash of birthday kids and wishes them a happy birthday. We'll be coding our method in the.
-#"Happy Birthday #{kids_name}! You are now #{age} years old!"
-
-birthday_kids = {
-  "Timmy" => 9,
-  "Sarah" => 6,
-  "Amanda" => 27
-}
-
-def happy_birthday(birthday_kids)
-  birthday_kids.each do |kids_name, age|
-    puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
-  end
-end
-
-happy_birthday(birthday_kids)
