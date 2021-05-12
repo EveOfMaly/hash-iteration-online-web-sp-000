7bbcbbfda0ad3ab737f46e26eae1746b4f4b84f7
John Kyeremeh <john.a.kyeremeh@gmail.com>
2021-05-12 11:10:44 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -1,14 +1,8 @@
-def happy_birthday(birthday_kids)
-  birthday_kids.each do |kids_name, age|
-    puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
-  end
-end
-
-happy_birthday(birthday_kids)
-
 
 # Our job is to write a method that operates on a hash of birthday kids and wishes them a happy birthday. We'll be coding our method in the.
 #"Happy Birthday #{kids_name}! You are now #{age} years old!"
+
+
 
 birthday_kids = {
   "Timmy" => 9,
@@ -17,4 +11,11 @@ birthday_kids = {
 }
 
 
+def happy_birthday(birthday_kids)
+  birthday_kids.each do |kids_name, age|
+    puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
+  end
+end
+
+happy_birthday(birthday_kids)
 
