8ae3fdeb0b31a82d299cdf0b80857444560668ff
John Kyeremeh <john.a.kyeremeh@gmail.com>
2021-05-12 10:57:07 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -6,6 +6,8 @@ suite_d: "Charlie Chaplin",
 suite_e: "Crumpet the Elf"
 }
 
+# The criteria for picking the winner is that this person must be staying in Suite A and their name must start with the letter "A".
+
 def select_winner(passengers)
   passengers.each do |k,v|
     puts k
